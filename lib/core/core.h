b1bb4b7b1f47ee4e9598d78e39f9b0dbaef38612
Abhishek Chakravarti <abhishek@taranjali.org>
2020-03-14 13:03:51 +0530

    Define inq_assert() macro
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- lib/core/core.h
+++ lib/core/core.h
@@ -18,5 +18,19 @@
 #endif
 
 
+#if !(defined NDEBUG)
+#   define inq_assert(p)                                               \
+    do {                                                               \
+        if (inq_unlikely (!(p))) {                                     \
+            printf("inq_assert() condition failed: %s [%s, %s, %d]\n", \
+                    #p, __func__, __FILE__, __LINE__);                 \
+            abort();                                                   \
+        }                                                              \
+    } while (0)
+#else
+#   define inq_assert(p)
+#endif
+
+
 #endif /* INQUERY_CORE_HEADER_INCLUDED */
 
