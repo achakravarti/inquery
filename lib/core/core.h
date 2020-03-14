cee101a152d8caf799f07595f36c0f8248971a00
Abhishek Chakravarti <abhishek@taranjali.org>
2020-03-14 13:38:01 +0530

    Create runtime check macros
    The runtime check macros `inq_assert()` and `inq_require()`
    have been introduced. The `inq_assert()` macro asserts at
    runtime that a given precondition is true, and aborts if
    the check fails. The `inq_require()` macro checks whether a
    post-condition is true, and exits with teh `EXIT_FAILURE`
    code if the check fails.
    
    Ideally, `inq_require()` should have access to an exception
    handler callback function that it can call in case of failure
    of the check; this is, however, deferred to a later issue.
    
    Additionally, the branch prediction hint macros `inq_likely()`
    and `inq_unlikely()` have also been defined as they are used
    by the runtime check macros. These branch prediction macros
    are modelled after the `likely()` and `unlikely()` macros.
    
    This commit squashes the following commits from the
    feat/runtime-checks branch:
      * 326ccb9eea2af412baf418581a095ec52df6f453
      * da7b8de638751892992ecacd68d348224703af83
      * 755846bde9aec66d028d01161afe77cc5bb84348
      * b1bb4b7b1f47ee4e9598d78e39f9b0dbaef38612
      * e93b2b574d5633acdbaee5c35402520529327beb
      * bb175a83382e3ff2d2bc47c783dcd426dd93033a
      * d6c6b05c99a05f29ac9e826786915e5fbc59daca
    
    This commit closes issue #3.
    
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

---
+++ lib/core/core.h
@@ -0,0 +1,85 @@
+/******************************************************************************
+ *                     ___                                   
+ *                    |_ _|_ __   __ _ _   _  ___ _ __ _   _ 
+ *                     | || '_ \ / _` | | | |/ _ \ '__| | | |
+ *                     | || | | | (_| | |_| |  __/ |  | |_| |
+ *                    |___|_| |_|\__, |\__,_|\___|_|   \__, |
+ *                                  |_|                |___/ 
+ *
+ * Inquery - India Query RESTful microservice
+ * Copyright (c) 2020 Abhishek Chakravarti <abhishek@taranjali.org>. 
+ * See the accompanying inquery/AUTHORS file for the full list of contributors.
+ *
+ * This is the inquery/lib/core.h header file; it declares the core interface
+ * on which the Inquery microservice is built.
+ *
+ * This code is released under the GPLv3 License. See the accompanying 
+ * inquery/LICENSE file or <http://opensource.org/licenses/GPL-3.0> for complete
+ * licensing details.
+ *
+ * BY CONTINUING TO USE AND/OR DISTRIBUTE THIS FILE, YOU ACKNOWLEDGE THAT YOU
+ * HAVE UNDERSTOOD THESE LICENSE TERMS AND ACCEPT THEM.
+ ******************************************************************************/
+
+
+#if (!defined INQUERY_CORE_HEADER_INCLUDED)
+#define INQUERY_CORE_HEADER_INCLUDED
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+
+/*
+ * inq_likely() - hint that a predicate is likely to be true
+ */
+#if (defined __GNUC__ || defined __clang__)
+#   define inq_likely(p) (__builtin_expect (!!(p), 1))
+#else
+#   define inq_likely(p) (p)
+#   warning "inq_likely() has no effect on non GCC-compatible C compilers"
+#endif
+
+
+/*
+ * inq_unlikely() - hint that a predicate is unlikely to be true
+ */
+#if (defined __GNUC__ || defined __clang__)
+#   define inq_unlikely(p) (__builtin_expect (!!(p), 0))
+#else
+#   define inq_unlikely(p) (p)
+#   warning "inq_likely() has no effect on non GCC-compatible C compilers"
+#endif
+
+
+/*
+ * inq_assert() - assert that a predicate is true
+ */
+#if !(defined NDEBUG)
+#   define inq_assert(p) do {                                          \
+        if (inq_unlikely (!(p))) {                                     \
+            printf("inq_assert() condition failed: %s [%s, %s, %d]\n", \
+                    #p, __func__, __FILE__, __LINE__);                 \
+            abort();                                                   \
+        }                                                              \
+    } while (0)
+#else
+#   define inq_assert(p) ((void 0)
+#endif
+
+
+/*
+ * inq_require() - ensure that a predicate is true
+ */
+#define inq_require(p) do {                                           \
+    if (inq_unlikely (!(p))) {                                        \
+        printf("inq_require() condition failed @ %s() [%s:%d]: %s\n", \
+                __func__, __FILE__, __LINE__, #p);                    \
+        exit(EXIT_FAILURE);                                           \
+    }                                                                 \
+} while (0)
+
+
+#endif /* INQUERY_CORE_HEADER_INCLUDED */
+
