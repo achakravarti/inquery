bb175a83382e3ff2d2bc47c783dcd426dd93033a
Abhishek Chakravarti <abhishek@taranjali.org>
2020-03-14 12:59:34 +0530

    Define inq_unlikely() macro
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- lib/core/core.h
+++ lib/core/core.h
@@ -8,5 +8,13 @@
 #   define inq_likely(p) (p)
 #endif
 
+
+#if (defined __GNUC__ || defined __clang__)
+#   define inq_unlikely(p) (__builtin_expect (!!(p), 0))
+#else
+#   define inq_unlikely(p) (p)
+#endif
+
+
 #endif /* INQUERY_CORE_HEADER_INCLUDED */
 
