f049965ed74c514e844614928c9d00fe4774e83f
Abhishek Chakravarti <abhishek@taranjali.org>
2020-03-15 23:54:59 +0530

    Add unit tests for object model interface
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- test/suite.h
+++ test/suite.h
@@ -6,5 +6,12 @@
  */
 extern void inquery_test_suite_heap(void);
 
+
+/*
+ * inquery_test_suite_object() - object model interface test suite
+ */
+extern void inquery_test_suite_object(void);
+
+
 #endif /* INQUERY_TEST_SUITE_HEADER */
 
